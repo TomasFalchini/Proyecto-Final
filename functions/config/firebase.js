ec1b01a70a12a7be763d825d9062024fc91e8f36
Tomás Falchini <108646907+TomasFalchini@users.noreply.github.com>
2022-11-22 22:50:23 -0300

    Delete firebase.js

--- functions/config/firebase.js
+++
@@ -1,7 +0,0 @@
-const admin = require("firebase-admin");
-const functions = require("firebase-functions");
-
-const db = admin.firestore();
-//COMENTARIO DE PRUEBA
-
-module.exports = { admin, db };
